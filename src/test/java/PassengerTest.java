76100662070af11095cfc4fef00c36a9eef68400
Cameron <cameron.beckit@gmail.com>
2020-01-12 21:38:05 +0000

    adds passenger tests

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    Flight flight1;
+    Passenger passenger1;
+
+    @Before
+    public void setup(){
+        flight1 = new Flight("FR756", "EDI","GLA","12:00");
+        passenger1 = new Passenger("Steve", 2);
+    }
+
+    @Test
+    public void canGetPassengerName(){
+        assertEquals("Steve", passenger1.getPassengerName());
+    }
+
+}
