e9fa171a0911bfcb195dcecb73c3dfc0b64efd89
Cameron <cameron.beckit@gmail.com>
2020-01-12 21:54:34 +0000

    adds plane tests

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,14 +1,14 @@
 public class Plane {
 
-    private PlaneType capacity;
-    private PlaneType totalWeight;
+    private PlaneType planeType;
 
-    public Plane(PlaneType capacity, PlaneType totalWeight) {
+
+    public Plane(PlaneType planeType) {
         this.capacity = capacity;
         this.totalWeight = totalWeight;
     }
 
     public int getCapacity(){
-        return this.capacity.getCapacity();
+        return this.PlaneType.getCapacity();
     }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,21 +1,21 @@
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class PlaneTest {
-//
-//    Plane plane1;
-//    Passenger passenger1;
-//
-//    @Before
-//    public void setup(){
-//        plane1 = new Plane();
-//        passenger1 = new Passenger("Steve", 2);
-//    }
-//
-//    @Test
-//    public void canGetCapacity(){
-//        assertEquals(10, Plane.getCapacity());
-//    }
-//}
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaneTest {
+
+    Plane plane1;
+    Passenger passenger1;
+
+    @Before
+    public void setup(){
+        plane1 = new Plane(PlaneType.BOEING747);
+        passenger1 = new Passenger("Steve", 2);
+    }
+
+    @Test
+    public void canGetCapacity(){
+        assertEquals(10, plane1.getCapacity());
+    }
+}
